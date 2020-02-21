4fea4e6a93d4a01f594ca722ef49548f78f9516d
ntan <nguyenthanhan1001@gmail.com>
2020-02-21 10:32:59 +0700

    update get file list

--- get_file_list.py
+++ get_file_list.py
@@ -6,4 +6,12 @@ g_login = GoogleAuth()
 g_login.LocalWebserverAuth()
 drive = GoogleDrive(g_login)
 
-file_list = drive.ListFile({'q': "'<folder ID>' in parents and trashed=false"}).GetList()
+file_list = drive.ListFile({'q': "'folder id' in parents and trashed=false"}).GetList()
+print(len(file_list))
+# print(file_list[0])
+file_list.sort()
+with open('file_ids.txt', 'wt') as f:
+    for it in file_list:
+        print it['title']+'.zip', it['id']
+        f.write(it['title']+'.zip' + '\t' + it['id'] + '\n')
+
